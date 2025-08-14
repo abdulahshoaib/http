b3b2ce4e7ca3e7958af976fd41ddb00b0b134a7a
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-08-14 06:00:42 +0500

    feat: constructing message out of \nw

--- main.go
+++ main.go
@@ -1,23 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
 )
 
-func main(){
+func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal("error:", err)
 	}
 
+	str := ""
 	for {
-		data:= make([]byte, 8)
-		n, err :=  f.Read(data)
+		data := make([]byte, 8)
+		n, err := f.Read(data)
 		if err != nil {
 			break
 		}
-		fmt.Printf("read: %s\n", data[:n])
+
+		data = data[:n]
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+		str += string(data)
+	}
+
+	if len(str) != 0 {
+		fmt.Printf("read: %s \n", str)
 	}
 }
