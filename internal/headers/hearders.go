9545f408594abea670597ab5a380ae22087aa76e
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-08-14 14:53:12 +0500

    feat: adding RFC Constraints

--- internal/headers/hearders.go
+++ internal/headers/hearders.go
@@ -3,14 +3,34 @@ package headers
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
-type Headers map[string]string
+func isValidToken(str string) bool {
+	for _, ch := range str {
+		res := false
+		if ch >= 'A' && ch <= 'Z' || ch >= 'a' && ch <= 'z' || ch >= '0' && ch <= '9' {
+			res = true
+		}
+		switch ch {
+		case '!', '#', '$', '%', '&', '\'', '*', '+', '-', '.', '^', '_', '`', '|', '~':
+			res = true
+		}
+
+		if !res {
+			return false
+		}
+	}
+
+	return true
+}
 
 var crlf = []byte("\r\n")
 
-func NewHeaders() Headers {
-	return map[string]string{}
+func NewHeaders() *Headers {
+	return &Headers{
+		headers: map[string]string{},
+	}
 }
 
 func parseHeader(fieldLines []byte) (string, string, error) {
@@ -27,6 +47,18 @@ func parseHeader(fieldLines []byte) (string, string, error) {
 	}
 
 	return string(name), string(val), nil
+}
+
+type Headers struct {
+	headers map[string]string
+}
+
+func (h Headers) Get(name string) string {
+	return h.headers[strings.ToLower(name)]
+}
+
+func (h Headers) Set(name, value string) {
+	h.headers[strings.ToLower(name)] = value
 }
 
 func (h Headers) Parse(data []byte) (int, bool, error) {
@@ -50,9 +82,13 @@ func (h Headers) Parse(data []byte) (int, bool, error) {
 		if err != nil {
 			return 0, false, err
 		}
+		if !isValidToken(name) {
+			return 0, false, fmt.Errorf("malformed header name")
+		}
+
 		read += idx + len(crlf)
 
-		h[name] = val
+		h.Set(name, val)
 	}
 
 	return read, done, nil
