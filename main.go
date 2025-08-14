bee8f963ffe365cb249172429f491c768830507f
abdulahshoaib <khan.abdullah.shoaib@gmail.com>
2025-08-14 05:48:09 +0500

    feat: reading data from a file, 8 bytes a time

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main(){
-	fmt.Println("I hope I got the job");
+	f, err := os.Open("messages.txt")
+	if err != nil {
+		log.Fatal("error:", err)
+	}
+
+	for {
+		data:= make([]byte, 8)
+		n, err :=  f.Read(data)
+		if err != nil {
+			break
+		}
+		fmt.Printf("read: %s\n", data[:n])
+	}
 }
